4c470b4cf39ff4ae4ca6486dfa743b2022dacd3a
Balázs Baumgartner <balazskaxd@gmail.com>
2019-12-30 12:33:45 +0100

    iso charset

--- android/src/main/java/leesiongchan/reactnativeescpos/PrinterService.java
+++ android/src/main/java/leesiongchan/reactnativeescpos/PrinterService.java
@@ -46,7 +46,7 @@ public class PrinterService {
     }
 
     public void print(String text) throws UnsupportedEncodingException {
-        write(text.getBytes("UTF8"));
+        write(text.getBytes("ISO-8859-1"));
     }
 
     public void printLn(String text) throws UnsupportedEncodingException {
@@ -255,7 +255,7 @@ public class PrinterService {
             }
 
             try {
-                baos.write(layoutBuilder.createFromDesign(line, charsOnLine).getBytes("UTF8"));
+                baos.write(layoutBuilder.createFromDesign(line, charsOnLine).getBytes("ISO-8859-1"));
             } catch (UnsupportedEncodingException e) {
                 // Do nothing?
             }
